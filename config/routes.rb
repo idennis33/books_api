d697617a17c352a63b7238cff748a1fcd01ebf62
Isaac Dennis <idennisgsu3@gmail.com>
2020-09-30 21:21:51 -0400

    got signup route to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :books
   resources :users do                                                            
   collection do                                                                
-    post '/login', to: 'users#login'                                            
+    post '/login', to: 'users#login'  
+    post '/create', to: 'users#create'                                          
   end                                  
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
